fe70accca60d0114841eebddb4ce0e7a08d383f2
MartinTschechne <mat@visium.ch>
2020-02-28 09:23:28 +0100

    Created project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for projects of the course "Machine Learning for Health Care".',
+    author='Martin Tschechne',
+    license='',
+)
