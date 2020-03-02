ef1685e7af3685bea0a3f574a31f1693bb17c688
MartinTschechne <mat@visium.ch>
2020-03-02 13:59:11 +0100

    Added yaml-configuration

--- ECG-time-series/models/models.py
+++ ECG-time-series/models/models.py
@@ -3,6 +3,7 @@ from keras.layers import Dense, Dropout, Embedding, SimpleRNN, LSTM, GRU, Flatte
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 
 def get_base_model(n_classes):
+    '''model_id: base-model'''
     model = Sequential()
     model.add(Conv1D(input_shape=(187,1),filters=32, kernel_size=5, padding='same', activation='relu'))
     model.add(Conv1D(filters=32, kernel_size=5, padding='same', activation='relu'))
@@ -15,9 +16,10 @@ def get_base_model(n_classes):
     model.add(Dense(64,activation='relu'))
     model.add(Dense(n_classes,activation='softmax'))
     model.summary()
-    return model, 'base_model'
+    return model
 
 def get_pure_LSTM(n_classes):
+    '''model_id: pure-LSTM'''
     model.add(LSTM(256,return_sequences=True,input_shape=(187,1)))
     model.add(LSTM(128,return_sequences=True))
     model.add(LSTM(64,return_sequences=True))
@@ -25,9 +27,10 @@ def get_pure_LSTM(n_classes):
     model.add(Dense(64,activation='relu'))
     model.add(Dense(n_classes,activation='softmax'))
     model.summary()
-    return model, 'lstm_model'
+    return model
 
 def get_debug_model(n_classes):
+    '''model_id: debug'''
     model = Sequential()
     model.add(Conv1D(input_shape=(187,1),filters=32, kernel_size=5, padding='same', activation='relu'))
     model.add(Conv1D(filters=32, kernel_size=5, padding='same', activation='relu'))
@@ -36,4 +39,15 @@ def get_debug_model(n_classes):
     model.add(Dense(64,activation='relu'))
     model.add(Dense(n_classes,activation='softmax'))
     model.summary()
-    return model, 'debug_model'
+    return model
+
+def get_model(model_id, n_classes):
+    if model_id == 'base-model':
+        return get_base_model(n_classes)
+    elif model_id == 'pure-LSTM':
+        return get_pure_LSTM(n_classes)
+    elif model_id == 'debug':
+        return get_debug_model(n_classes)
+    else:
+        print("Please specify a model.")
+        exit(-1)
--- ECG-time-series/train.py
+++ ECG-time-series/train.py
@@ -1,31 +1,47 @@
+import argparse
 import numpy as np
-from data.make_dataset import load_mitbih, upsample
-from models.models import get_base_model, get_debug_model
+from data.make_dataset import load_mitbih, load_ptbdb, upsample
+from models.models import get_model
 from keras import optimizers
 from keras.losses import SparseCategoricalCrossentropy
 from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 import pickle
-
+import yaml
 
 def main():
+    ### load config ###
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Specify path to yaml config file.')
+    args = parser.parse_args()
+    try:
+        with open(args.config,'r') as file:
+            config = yaml.safe_load(file)
+    except Exception as e:
+        print('Error reading the config file')
+
     ### load & preprocess data ###
     print("Load data ...")
-    train, test, y_train, y_test = load_mitbih()
+    if config['dataset'] == 'mitbih':
+        train, test, y_train, y_test = load_mitbih()
+    elif conig['dataset'] == 'ptbdb':
+        train, test, y_train, y_test = load_ptbdb()
+
     N_CLASSES = len(np.unique(y_train))
     data_dim = train.shape
     train, val, y_train, y_val = train_test_split(train, y_train,
-                                                test_size=0.2,
+                                                test_size=config['val_split_size'],
                                                 stratify=y_train)
-    train_up, y_train_up = upsample(train, y_train)
+    if config['upsample']:
+        train_up, y_train_up = upsample(train, y_train)
 
     ### define model ###
-    model, model_name = get_base_model(n_classes=N_CLASSES)
-    model.save(f'./{model_name}.h5')
+    model = get_model(config['model_id'],N_CLASSES)
+    model.save(f"./{config['experiment_name']}.h5")
 
     model_yaml = model.to_yaml()
-    with open(f"{model_name}.yaml", "w") as yaml_file:
+    with open(f"{config['experiment_name']}.yaml", "w") as yaml_file:
         yaml_file.write(model_yaml)
 
     opt = optimizers.Adam(clipnorm=1.)
@@ -34,19 +50,20 @@ def main():
                   metrics=['sparse_categorical_accuracy'])
 
     ### train ###
-    model_path = f'{model_name}-weights.h5'
+    model_path = f"{config['experiment_name']}-weights.h5"
     checkpoint = ModelCheckpoint(model_path, monitor='val_loss', save_weights_only=True,save_best_only=True, mode='min')
     early = EarlyStopping(monitor="val_loss", mode="min", patience=5, verbose=1)
-    csv_logger = CSVLogger(f'{model_name}.log')
+    csv_logger = CSVLogger(f"{config['experiment_name']}.log")
     callbacks_list = [checkpoint, early, csv_logger]
 
     ### for debugging only
-    #train_up, _, y_train_up, _ = train_test_split(train_up, y_train_up,train_size=100)
+    if config['debug']:
+        train_up, _, y_train_up, _ = train_test_split(train_up, y_train_up,train_size=100)
 
     print('Start training ...')
     history = model.fit(train_up, y_train_up,
-                        batch_size=128,
-                        epochs=1,
+                        batch_size=config['batch_size'],
+                        epochs=config['epochs'],
                         shuffle=True,
                         validation_data = (val,y_val),
                         callbacks=callbacks_list)
@@ -57,7 +74,7 @@ def main():
     model.load_weights(model_path)
     pred_test = model.predict(test)
     try:
-        pickle.dump(pred_test,open(f'{model_name}-preds.pkl','wb'))
+        pickle.dump(pred_test,open(f"{config['experiment_name']}-preds.pkl",'wb'))
     except Exception as e:
         print(e)
     pred_test = np.argmax(pred_test, axis=1)
